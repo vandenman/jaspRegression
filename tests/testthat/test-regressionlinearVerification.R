a1fbb3149d51a475a01080189839c4c215e25567
Don van den Bergh <donvdbergh@hotmail.com>
2025-10-15 11:00:49 +0200

    add new columns in verified test output (existing values are unchanged)

--- tests/testthat/test-regressionlinearVerification.R
+++ tests/testthat/test-regressionlinearVerification.R
@@ -24,17 +24,18 @@ options$residualsSavedToData <- FALSE
 options$residualsSavedToDataColumn <- FALSE
 options$residualStatistic <- FALSE
 
-results <- jaspTools::runAnalysis("RegressionLinear", "Regression.csv", options)
+results <- jaspTools::runAnalysis("RegressionLinear", testthat::test_path("Regression.csv"), options)
 
 # https://jasp-stats.github.io/jasp-verification-project/regression.html#linear-regression
 test_that("Main table results match R, SPSS, SAS and MiniTab", {
   # Main table
   resultTable <- results$results$modelContainer$collection$modelContainer_summaryTable$data
-  
+
   jaspTools::expect_equal_tables(
     "test"=resultTable,
-    "ref"=list(0, 0, 11.711593246071, 0, "H<unicode>", 0.806436535963374, 0.650339886536607,
-            6.96410449545021, 0.646411121216794, "H<unicode>")
+    "ref"=list(0, 0, 0, 11.711593246071, 0, 0, 90, "M<unicode>", "", 0.806436535963375,
+               0.650339886536607, 0.650339886536607, 6.96410449545021, 0.646411121216794,
+               1, 89, "M<unicode>", 5.11889396069709e-22)
   )
 })
 
@@ -42,7 +43,7 @@ test_that("Main table results match R, SPSS, SAS and MiniTab", {
 test_that("ANOVA table results match R, SPSS, SAS and MiniTab", {
   # ANOVA table
   resultTable <- results$results$modelContainer$collection$modelContainer_anovaTable$data
-  
+
   jaspTools::expect_equal_tables(
     "test"=resultTable,
     "ref"=list("TRUE", 165.532892294899, 8028.13859583154, 8028.13859583154,
@@ -56,7 +57,7 @@ test_that("ANOVA table results match R, SPSS, SAS and MiniTab", {
 test_that("Coefficient table results match R, SPSS, SAS and MiniTab", {
   # Coefficient table
   resultTable <- results$results$modelContainer$collection$modelContainer_coeffTable$data
-  
+
   jaspTools::expect_equal_tables(
     "test"=resultTable,
     "ref"=list("FALSE", 1.22770856137111, "H<unicode>", "(Intercept)", 6.22465705469213e-34,
@@ -72,7 +73,7 @@ test_that("Coefficient table results match R, SPSS, SAS and MiniTab", {
 test_that("Descriptive table results match R, SPSS, SAS and MiniTab", {
   # Descriptives table
   resultTable <- results$results$modelContainer$collection$modelContainer_descriptivesTable$data
-  
+
   jaspTools::expect_equal_tables(
     "test"=resultTable,
     "ref"=list(91, 11.711593246071, 1.22770856137111, 23.8351648351648, "Y",
